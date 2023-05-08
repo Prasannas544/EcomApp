fac22e815ea707cff89bbbdfbc24bb263ce08d22
Mishratapas <mishratapas769@gmail.com>
2023-05-09 01:55:05 +0530

    bug in navigator

---
+++ src/screens/Auth/LogInScreen.jsx
@@ -0,0 +1,21 @@
+import {View, Text, SafeAreaView, StatusBar} from 'react-native';
+import React from 'react';
+
+const LogInScreen = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#000'} />
+      <View>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+        <Text>LogInScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LogInScreen;
